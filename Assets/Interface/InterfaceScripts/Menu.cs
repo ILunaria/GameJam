080ad09505968c67104cad85b5c3d338fa679ec3
ILunaria <XxantxX752@gmail.com>
2022-10-02 16:42:38 -0400

    Atualizado

--- Assets/Interface/InterfaceScripts/Menu.cs
+++ Assets/Interface/InterfaceScripts/Menu.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    private void Awake()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
     public void Jogar()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
