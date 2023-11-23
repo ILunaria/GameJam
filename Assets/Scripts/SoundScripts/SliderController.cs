9ea04ae6d029a09983e0b26ebbfea9f2ab815b5b
MrWizardry <pedroaugustottyyt@gmail.com>
2023-11-22 20:59:39 -0400

    Sistema de Slider e atual do Code

---
+++ Assets/Scripts/SoundScripts/SliderController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SliderController : MonoBehaviour
+{
+    public AudioMixerGroup audioMixerGroup;
+    public Slider slider;
+
+    private string volumePlayerPrefsKey = "MusicVolume";
+
+    void Start()
+    {
+        SetVolume(slider.value);
+    }
+
+    void Awake()
+    {
+        slider.onValueChanged.AddListener(SetVolume);
+        float savedVolume = PlayerPrefs.GetFloat(volumePlayerPrefsKey, slider.value);
+        slider.value = savedVolume;
+        SetVolume(savedVolume);
+    }
+
+    void SetVolume(float volume)
+    {
+        audioMixerGroup.audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+
+        PlayerPrefs.SetFloat(volumePlayerPrefsKey, volume);
+        PlayerPrefs.Save();
+    }
+    
+}
